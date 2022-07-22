8782873ed2c8d6b860a8bea73bd630f8f5d3339c
Victor Edmundo Valenzuela Trujillo <vvalenzuelat@tpc.cl>
2022-07-21 22:21:22 -0400

    primer commit del proyecto

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Primero from './Componentes/Primero';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+
+  constructor(props){
+    super(props);
+    this.state={
+      nombre:"",
+      apellido:""
+    }
+  }
+
+  clickMe = (e) =>{
+    alert('este evento corresponde a un click pero como función.');
+  }
+
+  cambiarEstado = (e) =>{
+    this.setState({
+      ...this.state,
+      [e.target.name]: e.target.value
+    })
+  }
+
+  formSubmit = (e) =>{
+    e.preventDefault();
+  }
+
+  render(){
+    return(      
+        <div className="App">
+
+          <h1>Primera tarea React</h1>
+          <form onSubmit={this.formSubmit}>
+            <label name='lblNombre'>Nombre: </label>
+            <input type='text' name='nombre' value={this.state.nombre} onChange={this.cambiarEstado}></input><br/>
+            <label name='lblApellido'>Apellido: </label>
+            <input type='text' name='apellido' value={this.state.apellido} onChange={this.cambiarEstado}></input><br/>
+            <button type='submit'>Enviar</button>
+            <button type='submit'>Limpiar</button>
+          </form>
+          {/*<button type='button' onClick={this.clickMe}>Click Me!!!</button>*/}
+          
+          <Primero titulo={'Este es un props'} objeto={{nombre: 'OBJ', tipo:'Cualquiera'}} nombre={this.state.nombre} apellido={this.state.apellido}>
+            <ul>
+              <li>Este es un Hijo</li>
+              <li>Este es otro Hijo</li>
+            </ul>
+          </Primero>
+        </div>      
+    );
+  }
 }
 
 export default App;
---
+++ src/Componentes/Primero.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+class Primero extends React.Component{
+    render(){
+        const { titulo,objeto, children: hijos} = this.props;
+
+        return(
+            <React.Fragment>                
+                <h3>La propiedad Titulo es: {titulo}</h3>
+                <h3>El nombre del objeto es: { objeto? objeto.nombre : '' }</h3>
+                <h1>El nombre es {this.props.nombre} y el apellido es {this.props.apellido}</h1>
+            </React.Fragment>
+        );
+    }
+}
+
+export default Primero;
